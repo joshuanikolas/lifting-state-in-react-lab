b4f6a96ad293052c6680f63735b0e9fe557817a9
joshuanikolas <nikolassnchez@icloud.com>
2024-11-22 02:05:24 -0600

    fixed code, now up and running

--- components/IngredientList.jsx
+++ components/IngredientList.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const IngredientList = (props) => {
     return ( 
     <ul>
-        {props.availableIngredients.map((openIngredient, index) => (
+        {props.availableIngredients.map((availableIngredient, index) => (
             <li key={index} style={{ backgroundColor: availableIngredient.color }}>{availableIngredient.name}
             <button onClick={() => props.addToBurger(availableIngredient)}>+</button>
             </li>
